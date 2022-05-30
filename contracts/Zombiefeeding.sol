482808ed35e7010f9ad7a4e874d0c66ccd2f97ef
"Saachi Kaup" <"saachi.kaup@gmail.com">
2022-05-30 11:50:41 +0530

    "Added feedOnKitty function"

--- contracts/Zombiefeeding.sol
+++ contracts/Zombiefeeding.sol
@@ -29,5 +29,11 @@ contract ZombieFeeding is ZombieFactory {
         uint newDna = (_targetDna + myZombie.dna) / 2;
         _createZombie("NoName", newDna);
     }
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        ( , , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
+    }
     
 }
