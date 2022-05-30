802c15d5cb79e166d9a638f1bddc09b722b95a8b
"Saachi Kaup" <"saachi.kaup@gmail.com">
2022-05-30 11:45:24 +0530

    "Added kitty interface object"

--- contracts/Zombiefeeding.sol
+++ contracts/Zombiefeeding.sol
@@ -3,10 +3,25 @@ pragma solidity >=0.7.0 <0.9.0;
 import "./Zombiefactory.sol";
 
 contract KittyInterface {
-    
+    function getKitty(uint256 _id) external view returns (
+        bool isGestating,
+        bool isReady,
+        uint256 cooldownIndex,
+        uint256 nextActionAt,
+        uint256 siringWithId,
+        uint256 birthTime,
+        uint256 matronId,
+        uint256 sireId,
+        uint256 generation,
+        uint256 genes
+    );
 }
 
 contract ZombieFeeding is ZombieFactory {
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(ownerZombieCount[msg.sender] == 0); //should be the same thing
         Zombie storage myZombie = zombies[_zombieId];
@@ -14,4 +29,5 @@ contract ZombieFeeding is ZombieFactory {
         uint newDna = (_targetDna + myZombie.dna) / 2;
         _createZombie("NoName", newDna);
     }
+    
 }
