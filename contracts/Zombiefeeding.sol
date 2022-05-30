9351dc7b4fbbe47732d98efab3b0bf3c87a9de89
"Saachi Kaup" <"saachi.kaup@gmail.com">
2022-05-30 11:38:15 +0530

    "Added KittyInterface Contract"

--- contracts/Zombiefeeding.sol
+++ contracts/Zombiefeeding.sol
@@ -2,6 +2,10 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "./Zombiefactory.sol";
 
+contract KittyInterface {
+    
+}
+
 contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(ownerZombieCount[msg.sender] == 0); //should be the same thing
